47e54d949e52ee38088497432a9c66b274ffd2ea
ashish <stuff.ashish@gmail.com>
2017-07-31 13:02:41 +0530

    First Commit Class using eclipse

---
+++ MyFirstJavaProject/src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+
+String name;
+int age;
+String height;
+String sex;
+
+	public static void main(String args[]) {
+		Person ob = new Person();
+		ob.name = "Geetika";
+		ob.height = "5ft5inchs";
+		ob.sex = "F";
+		
+		System.out.println("Name = Geetika");
+		System.out.println("Age = 26");
+		System.out.println("Sex = F");
+		
+	}
+}
